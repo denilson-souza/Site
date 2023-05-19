45fc73407739b9e2fc97a4df327215dfeaeba934
Denilson de Souza <souzavig93@gmail.com>
2023-05-19 18:30:14 -0300

    first commit

---
+++ JS/animacao.js
@@ -0,0 +1,20 @@
+/*MENU MOBILE */
+document.querySelector(".abrirMenu").onclick = function(){
+    document.documentElement.classList.add("menuAtivo");
+}
+document.querySelector(".fecharMenu").onclick = function(){
+    document.documentElement.classList.remove("menuAtivo");
+}
+document.querySelector(".fechar").onclick = function (){
+    document.documentElement.classList.remove("menuAtivo");
+}
+
+/*BANNER*/
+$(document).ready(function(){
+    $('.banner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+  });
